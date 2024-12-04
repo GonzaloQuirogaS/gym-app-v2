32038e34f71b8455cddfe79f84dde983e23785e1
Gonzalo Quiroga <gonzaquirogasassu@gmail.com>
2024-12-04 16:06:11 -0300

    6. Creacion y configuracion de microservicio Activity.

---
+++ microservice-activity/src/test/java/com/microservice/activity/MicroserviceActivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.activity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceActivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
