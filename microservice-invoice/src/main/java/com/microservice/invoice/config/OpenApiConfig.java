8e9500ee9582c9e60bc4bd21420036f8a84947f8
Gonzalo Quiroga <gonzaquirogasassu@gmail.com>
2024-12-21 21:04:12 -0300

    10.5 Implementacion de Swagger en rutas de microservicio Invoice

---
+++ microservice-invoice/src/main/java/com/microservice/invoice/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package com.microservice.invoice.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Gym management",
+                version = "2.0.0",
+                description = "Gym management application, invoice service"
+
+        ),
+        servers = {
+                @Server(url = "http://localhost:9080"),
+        }
+)
+public class OpenApiConfig {
+}
--- microservice-invoice/src/main/java/com/microservice/invoice/presentation/controller/InvoiceController.java
+++ microservice-invoice/src/main/java/com/microservice/invoice/presentation/controller/InvoiceController.java
@@ -3,6 +3,8 @@ package com.microservice.invoice.presentation.controller;
 import com.microservice.invoice.presentation.dto.InvoiceDto;
 import com.microservice.invoice.presentation.dto.InvoiceRequestDto;
 import com.microservice.invoice.service.interfaces.IInvoiceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,12 +20,18 @@ public class InvoiceController {
 
     private final IInvoiceService invoiceService;
 
+    @Tag(name = "GET", description = "Get methods")
+    @Operation(summary = "Get all invoices",
+            description = "Get all invoices")
     @GetMapping
     public ResponseEntity<List<InvoiceDto>> findAll() {
         List<InvoiceDto> invoices = invoiceService.findAll();
         return ResponseEntity.ok(invoices);
     }
 
+    @Tag(name = "GET")
+    @Operation(summary = "Get invoice by ID",
+            description = "Get invoice by ID")
     @GetMapping("/{id}")
     ResponseEntity<?> findInvoiceById(@PathVariable Long id) {
         try {
@@ -35,6 +43,9 @@ public class InvoiceController {
         }
     }
 
+    @Tag(name = "POST", description = "Post Methods")
+    @Operation(summary = "Save invoice",
+            description = "Save invoice")
     @PostMapping("/save")
     private ResponseEntity<?> save(@RequestBody InvoiceRequestDto invoiceRequestDto) {
         try {
@@ -46,8 +57,11 @@ public class InvoiceController {
         }
     }
 
+    @Tag(name = "DELETE")
+    @Operation(summary = "Delete invoice by ID",
+            description = "Delete invoice by ID")
     @DeleteMapping("/delete/{id}")
-    private ResponseEntity<?> delete(@RequestBody Long id) {
+    private ResponseEntity<?> delete(@PathVariable Long id) {
         try {
             InvoiceDto invoiceDto = invoiceService.findById(id);
             invoiceService.deleteById(id);
